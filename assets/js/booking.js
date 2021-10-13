81569a3005f5c9bda170c9e1340421364cbdb41a
mikk7021 <82369420+mikk7021@users.noreply.github.com>
2021-10-13 12:21:33 +0200

    Updated Booking site

---
+++ assets/js/booking.js
@@ -0,0 +1,52 @@
+const btn = document.querySelector('#submit2newsletter');
+
+const errorWrapper = document.querySelector('#error-wrapper');
+
+function removetext() {
+errorWrapper.innerHTML = null;
+}
+
+let select = document.getElementById('selecter')
+let email = document.getElementById('email')
+let inputdate = document.getElementById('inputdate')
+let number = document.getElementById('number')
+
+
+
+function validate () {
+
+    
+if (inputdate.value === ""){
+errorWrapper.innerHTML = "Vælg en tid i kallender"
+}
+    
+else if (select.value === ""){
+errorWrapper.innerHTML = "Vælg en behandling"
+}
+
+else if (email.value === "" || !isValidEmail(email.value)){
+errorWrapper.innerHTML = "Email er ikke korrekt"
+}
+
+else if (number.value === "" || !isValidNumber(number.value)){
+errorWrapper.innerHTML = "TLF nummer er ikke korrekt"
+}
+
+
+
+else {
+errorWrapper.innerHTML = " Formularen er afsendt"
+}
+}
+
+//Tjekker om værdi har en gyldig email syntaks
+function isValidEmail(value) {
+let pattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+return pattern.test(value);
+}
+
+//Tjekker om værdi er 8 tal
+function isValidNumber(value) {
+let pattern = /[0-9]{8}/;
+return pattern.test(value);
+}
